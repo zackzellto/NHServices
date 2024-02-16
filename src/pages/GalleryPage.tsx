df26101e94c8853c55630f66a4a652c6387e702f
zackzellto <zacharyzellto@gmail.com>
2024-02-15 23:49:27 -0700

    gallery page coming along!!

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Footer from "../components/Footer/Footer";
 import NHServicesLogo from "../images/logos/nhservices-logo.png";
+import { Button } from "@nextui-org/react";
 
 // Sample array of image URLs - replace these with your actual image URLs
 const imageArray = new Array(50)
@@ -26,24 +27,34 @@ const GalleryPage = () => {
           className="h-36 w-auto mx-auto"
         />
       </div>
-      <h2 className="text-center font-bold underline text-4xl mb-4">Gallery</h2>
-      <div className="max-h-[calc(100vh-200px)] overflow-y-auto p-4 grid grid-cols-4 gap-4 justify-center">
+      <h2 className="text-center font-bold text-4xl mb-4">Gallery</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-4 bg-gray-300 rounded-xl gap-4 justify-center p-2">
         {imageArray.slice(0, visibleImages).map((imageUrl, index) => (
-          <div key={index}>
+          <div key={index} className="mb-4">
             <img
               src={imageUrl}
               alt={`Gallery ${index + 1}`}
-              className="w-full h-auto p-2"
+              className="w-full h-auto drop-shadow-xl p-2 mx-auto sm:w-[350px] sm:h-550px"
             />
           </div>
         ))}
       </div>
       {visibleImages < imageArray.length && (
         <div className="text-center my-4">
-          <button onClick={loadMoreImages}>Load More</button>
+          <Button
+            className="w-1/2 sm:w-1/4 text-white font-semibold h-12 shadow-lg text-lg"
+            style={{
+              backgroundImage: "linear-gradient(to right,#54a0d7, #e75909 )",
+            }}
+            onClick={loadMoreImages}
+          >
+            Load More
+          </Button>
         </div>
       )}
-      <Footer className="fixed bottom-0 left-0 w-full" />
+      <div className="mt-auto">
+        <Footer />
+      </div>
     </div>
   );
 };
