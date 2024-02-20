6dc9c45574d9f2581b285c5b8a08008e9053cc85
zackzellto <zacharyzellto@gmail.com>
2024-02-19 17:13:59 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxying API requests to the Express server
       "/api": {
-        target: "http://localhost:5000", // Target backend server
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // If you are using https, set to true
-        ws: true, // Proxy websockets if needed
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
       },
     },
   },
